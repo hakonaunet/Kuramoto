cca67b413fff6fe4efd0db9765d3f4964927042d
hakonaunet <hakonaunet@hotmail.com>
2023-11-29 17:34:56 +0100

    TGUI removed, ImGui added. First compilation.

--- Makefile
+++ Makefile
@@ -1,30 +1,34 @@
 # Compiler and Flags
 CXX = g++
-CXXFLAGS = -std=c++20 -Wall
+CXXFLAGS = -std=c++20 -Wall -I/usr/local/include -I./imgui -I./imgui-sfml -Iinclude
 
 # Directories
 SRC_DIR = src
 OBJ_DIR = obj
-LIB_DIR = lib
 INCLUDE_DIR = include
 
 # Libraries
 SFML = -lsfml-graphics -lsfml-window -lsfml-system
-LIBS = $(SFML)
+OPENGL_LIBS = -lGL
+LIBS = -L/usr/local/lib $(SFML) $(OPENGL_LIBS)
 
 # All cpp and corresponding obj files
+IMGUI_SRC = $(wildcard ./imgui/*.cpp) $(wildcard ./imgui-sfml/*.cpp)
 SRC = $(wildcard $(SRC_DIR)/*.cpp)
-OBJ = $(SRC:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o)
+OBJ = $(SRC:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o) $(IMGUI_SRC:%.cpp=%.o)
 
 # Executable Name
 EXEC = Kuramoto
 
 # Rules
 all: $(OBJ)
-	$(CXX) $^ $(LIBS) -o $(EXEC)
+	$(CXX) $(OBJ) $(LIBS) -o $(EXEC)
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	$(CXX) $(CXXFLAGS) -I$(INCLUDE_DIR) -c $< -o $@
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 clean:
-	rm $(OBJ_DIR)/*.o $(EXEC)
+	-rm -f $(OBJ_DIR)/*.o $(EXEC) *.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,34 @@
-// main.cpp
 #include "Grid.hpp"
 #include "OscillatorView.hpp"
 
+#include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Sleep.hpp>
-#include <SFML/Graphics.hpp>
+#include <../imgui/imgui.h>
+#include <../imgui-sfml/imgui-SFML.h>
 
 #include <ctime>
 
 int main() {
-    srand(time(NULL));  // Set seed for random number generator
+    srand(time(NULL)); // Set seed for random number generator
 
     int gridWidth = 40, gridHeight = 40;
-    int monitorWidth = 1400, monitorHeight = 1400;
-    
-    // Calculate maximum allowed size for each oscillator based on monitor resolution
-    int maxOscillatorWidth = monitorWidth / gridWidth;
-    int maxOscillatorHeight = monitorHeight / gridHeight;
+    int uiWidth = 200; // Width of the UI column
+    int monitorWidth = 1400 + uiWidth, monitorHeight = 1400;
 
-    // Use the smaller of the two to ensure oscillators fit on the screen for both width and height
+    // Calculate maximum allowed size for each oscillator based on monitor resolution
+    int maxOscillatorWidth = (monitorWidth - uiWidth) / gridWidth;
+    int maxOscillatorHeight = monitorHeight / gridHeight;
     int oscillatorSize = std::min(maxOscillatorWidth, maxOscillatorHeight);
 
-    sf::RenderWindow window(sf::VideoMode(gridWidth * oscillatorSize, gridHeight * oscillatorSize), "Oscillator Grid");
+    sf::RenderWindow window(sf::VideoMode(monitorWidth, monitorHeight), "Oscillator Grid");
+    ImGui::SFML::Init(window); // Initialize ImGui with SFML
 
     Grid grid(gridWidth, gridHeight, Coupling::Uniform);
     OscillatorView view(grid, oscillatorSize);
 
     sf::Clock clock; // Clock to measure time between frames
-    const sf::Time MinFrameTime = sf::microseconds(1000); // minimum time between frames in microseconds
+    sf::Clock deltaClock; // Clock for tracking the time between ImGui frames
 
     while (window.isOpen()) {
         sf::Event event;
@@ -35,20 +36,36 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
+            ImGui::SFML::ProcessEvent(event); // Pass the event to ImGui
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart()); // Update ImGui
+
+        // ImGui UI code goes here
+        ImGui::Begin("Simulation Control");
+        if (ImGui::Button("Run Simulation")) {
+            // Add logic to run simulation
+        }
+        // Additional controls can be added here
+        ImGui::End();
 
         window.clear();
         view.render(window); // Render the current state of the grid
-        window.display();   // Display the rendered frame
+
+        ImGui::SFML::Render(window); // Render ImGui over SFML
+
+        window.display(); // Display the rendered frame
 
         // Ensure minimum frame time
-        while (clock.getElapsedTime() < MinFrameTime) {
-            sf::sleep(MinFrameTime - clock.getElapsedTime());
+        while (clock.getElapsedTime() < sf::microseconds(1000)) {
+            sf::sleep(sf::microseconds(1000) - clock.getElapsedTime());
         }
         clock.restart(); // Reset the clock for the next frame
 
-        grid.updateGrid(); // Now, update the grid for the next frame
+        grid.updateGrid(); // Update the grid for the next frame
     }
 
+    ImGui::SFML::Shutdown(); // Shutdown ImGui properly
     return 0;
 }
